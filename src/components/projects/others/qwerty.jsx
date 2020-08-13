62d3ad40545543785c9d93d7aac68b6cbe5600c8
Brittany Hasty <Brittany.R.Hasty@gmail.com>
2020-08-12 21:36:19 -0700

    Integrate Github's 3d model viewer

--- src/components/projects/others/qwerty.jsx
+++ src/components/projects/others/qwerty.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import QwertyModel from './QWERTY.stl'
 
 class Qwerty extends React.Component {
     render() {
@@ -9,7 +8,7 @@ class Qwerty extends React.Component {
                 <p>This is a side project I have been working on in my free time. Once fully coded, QWERTY will be an interactable AI pet fox with the ability to learn new people, objects, and environments.</p>
                 <br></br>
                 <div className="content">
-                    
+                    <iframe height='420' width='620' frameborder='0' src='https://render.githubusercontent.com/view/3d?url=https://raw.githubusercontent.com/brittany-hasty/hasty-website/master/src/components/projects/others/QWERTY.stl' title='QWERTY.stl'></iframe>
                 </div>
                 <br></br>
             </div>
