582c401ac15823ff2b88d4a349929338b60457a6
Brittany Hasty <Brittany.R.Hasty@gmail.com>
2020-08-24 15:27:31 -0700

    Add color to links

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -12,9 +12,9 @@ class Footer extends React.Component {
                 <h4>About</h4>
                 <p><em>My name is Brittany Hasty, and I am a software engineer in San Francisco. To learn more about what I've worked on, please visit my "About" or "Projects" page. </em></p>
                 <div>
-                    <a href="https://github.com/Brittany-Hasty"><img src={githubIcon} alt="GitHub" title="github" /></a>
-                    <a href="https://www.linkedin.com/in/brittany-hasty-548317163/"><img src={linkedinIcon} alt="LinkedIn" title="linkedin" /></a>
-                    <a href="https://www.youtube.com/channel/UCkQHfRS7yBZY6xArGbsF_RA"><img src={youtubeIcon} alt="Youtube" title="youtube" /></a>
+                    <a href="https://github.com/Brittany-Hasty"><img src={githubIcon} alt="GitHub" title="Github" /></a>
+                    <a href="https://www.linkedin.com/in/brittany-hasty-548317163/"><img src={linkedinIcon} alt="LinkedIn" title="Linkedin" /></a>
+                    <a href="https://www.youtube.com/channel/UCkQHfRS7yBZY6xArGbsF_RA"><img src={youtubeIcon} alt="Youtube" title="Youtube" /></a>
                 </div>
                 Copyright © 2020 <Link to="/" className="footer-link">Hasty Creations</Link>
             </footer>
