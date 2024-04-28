d762e10f3568c5adeb09e4291382bf97ac0b4ec3
Cody Hasty <codyrhasty@gmail.com>
2024-04-28 16:51:32 -0700

    feat: change wording on about me blurb

--- app/components/about/about-blurb.tsx
+++ app/components/about/about-blurb.tsx
@@ -2,7 +2,7 @@ export default function AboutBlurbComponent() {
   return (
     <div className="">
       {/* TODO: Short about me section, top 3 things to know */}
-      <span>Hey, I&apos;m Cody Hasty. I&apos;m a software engineer and web developer.</span>
+      <span>Hey, I&apos;m Cody Hasty. I&apos;m a software engineer based in San Francisco.</span>
     </div>
   )
 }
