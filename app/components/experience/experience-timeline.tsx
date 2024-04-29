da5c703c294ee17d4fd6cc30e518c9f6ac40b764
Cody Hasty <codyrhasty@gmail.com>
2024-04-28 23:32:16 -0700

    feat: add career information to the experience timeline

--- app/components/experience/experience-timeline.tsx
+++ app/components/experience/experience-timeline.tsx
@@ -22,7 +22,7 @@ import appAcademyLogo from '@public/images/experience/AppAcademy-Logo.png'
 
 export function useSectionInView() {
   const { ref, inView } = useInView({
-    threshold: 0.5,
+    threshold: 0.25,
   })
 
   return {
@@ -59,6 +59,11 @@ export default function ExperienceTimeline() {
             location='San Francisco, CA'
             company='Instrumentl'
             image={tlLogo}
+            highlights={[
+              "",
+              "",
+              ""
+            ]}
           />
         </VerticalTimelineElement>
       </div>
@@ -76,6 +81,14 @@ export default function ExperienceTimeline() {
             location='Oakland, CA'
             company='Pandora / SiriusXM'
             image={pandoraLogo}
+            highlights={[
+              "Migrated company's CI/CD pipeline from Jenkins to Github Actions with ”living documentation” auto-deployed to Github Pages",
+              "Enacted an alternative CRUD interface for a command-line tool used by multiple teams",
+              "Supported the release process by running automated tests on mobile and web platforms followed by signing off with manual testers",
+              "Integrated a Java to Typescript React app plugin to display a custom Android SDUI layout inspector",
+              "Evaluated multiple blockers preventing Espresso from returning accurate data within a Jenkins CI/CD pipeline",
+              "Ensured new steps to track analytics and outages impacting media playback"
+            ]}
           />
         </VerticalTimelineElement>
       </div>
@@ -90,10 +103,14 @@ export default function ExperienceTimeline() {
           visible={appAcademyEduInView.inView}
         >
           <TimelineElementComponent
-            title='Student'
+            title='Software Engineering Graduate'
             location='San Francisco, CA'
             company='App Academy'
             image={appAcademyLogo}
+            highlights={[
+              "Curriculum of Study in Web Development (Ruby on Rails and Javascript)",
+              "Rigorous 1000+ hour software development course with a <3% acceptance rate which encompasses full-stack development: Ruby On Rails, SQL, Javascript, React, TDD, algorithms, design patterns, and programming best practices."
+            ]}
           />
         </VerticalTimelineElement>
       </div>
@@ -111,6 +128,11 @@ export default function ExperienceTimeline() {
             location='San Francisco, CA'
             company='Volley Inc'
             image={volleyLogo}
+            highlights={[
+              "Analyzed data collected through AWS Lambda, Amplitude, and Amazon Cloudwatch logs for consistencies between bugs",
+              "Refined an Airtable database containing thousands of songs by removing duplicates and updating incorrect metadata",
+              "Enforced a high standard of quality by catching over a dozen code-breaking bugs during product testing"
+            ]}
           />
         </VerticalTimelineElement>
       </div>
@@ -128,6 +150,17 @@ export default function ExperienceTimeline() {
             location='Portsmouth, VA'
             company='MYMIC LLC'
             image={mymicLogo}
+            highlights={[
+              "Developed two cross - platform augmented reality apps using Unity and Vuforia in partnership with the US Air Force",
+              "Constructed 3D models of heavy equipment and animated them in Blender for visual tutorials of repair and maintenance",
+              "Collaborated with a team to attend conferences, deliver demos, and provide updates to the NSF",
+              "Maintained the company's SQL databases for their iOS and Android apps, containing daily activity for over 90 companies",
+              "Designed and implemented scannable barcodes through augmented reality for use with mobile apps for ease of use",
+              "Resolved user requests for new features in the company's online OSHA training programs",
+              "Completed major updates to the HTML and CSS content of all company websites",
+              "Cataloged company websites based on domain name, hosting services, and content management system",
+              "Documented all company OSHA training videos with url, host platform, and duration in extensive Excel spreadsheets"
+            ]}
           />
         </VerticalTimelineElement>
       </div>
@@ -145,6 +178,11 @@ export default function ExperienceTimeline() {
             location='Norfolk, VA'
             company='Old Dominion University Department of Computer Science'
             image={oduLogo}
+            highlights={[
+              "Published and presented findings of MRC bezier curve fitting using seven different algorithms of weighting inner - protein angles",
+              "Leveraged Python scripts in UCSF Chimera to demonstrate lateral and longitudinal discrepancies from protein analysis",
+              "Extended the Common Bioinformatics Library repository for PDB and MRC manipulation in C++"
+            ]}
           />
         </VerticalTimelineElement>
       </div>
@@ -163,6 +201,10 @@ export default function ExperienceTimeline() {
             location='Norfolk, VA'
             company='Old Dominion University'
             image={oduLogo}
+            highlights={[
+              "93 Credit Hours in Computer Engineering / Modeling and Simulation Engineering",
+              "Included courses: CS250 (Problem Solving and Programming), CS252 (Intro to Unix), CS361 (Advanced Data Structures and Algorithms), CS330 (Object Oriented Programming and Design), CS495 (Machine Learning for Pattern Recognition within Molecular Imaging), MSIM205 (Discrete Event Simulation), and MSIM320 (Continuous Simulation)."
+            ]}
           />
         </VerticalTimelineElement>
       </div>
--- app/components/experience/timeline-element.tsx
+++ app/components/experience/timeline-element.tsx
@@ -5,21 +5,29 @@ type TimelineElementProps = {
   title: string,
   location: string,
   company: string,
-  image?: StaticImageData
+  image?: StaticImageData,
+  highlights?: string[]
 }
 
-export default function TimelineElementComponent({ title, location, company, image }: TimelineElementProps) {
+export default function TimelineElementComponent({ title, location, company, image, highlights = [] }: TimelineElementProps) {
   return (
-    <div className="flex flex-row gap-x-6">
-      <div className="basis-3/4">
-        <h5 className="m-0 mb-2">{`${title} @ ${company}`}</h5>
-        <p className="text-neutral-500 m-0">{location}</p>
-      </div>
-      {image &&
-        <div className="self-center basis-1/4 min-w-36 lg:min-w-32">
-          <Image src={image} alt={`${company} Logo`} />
+    <div>
+      <div className="flex flex-row gap-x-6">
+        <div className="basis-3/4">
+          <h5 className="m-0 mb-2">{`${title} @ ${company}`}</h5>
+          <p className="text-neutral-500 m-0">{location}</p>
         </div>
-      }
+        {image &&
+          <div className="basis-1/4 my-1 min-w-36 lg:min-w-32">
+            <Image src={image} alt={`${company} Logo`} />
+          </div>
+        }
+      </div>
+      <ul className="list-disc">
+        {highlights.map((highlight: string, index: number) => (
+          <li key={index} className="text-neutral-500">{highlight}</li>
+        ))}
+      </ul>
     </div>
   )
 }
