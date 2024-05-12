c72298fe6c7a2b091a540209be0f26d652507b5c
Cody Hasty <codyrhasty@gmail.com>
2024-05-12 15:56:14 -0700

    feat: add basic contact form

--- app/components/contact-form.tsx
+++ app/components/contact-form.tsx
@@ -1,7 +1,68 @@
+'use client'
+
+import { useForm } from 'react-hook-form';
+import { sendEmail } from '@/utils/sendEmail';
+
+export type ContactFormData = {
+  name: string;
+  email: string;
+  message: string;
+};
+
 export default function ContactFormComponent() {
+  const { register, handleSubmit } = useForm<ContactFormData>()
+
+  function onSubmit(data: ContactFormData) {
+    sendEmail(data)
+  }
+
   return (
-    <div className="">
-      <div>TODO: CTA / Contact me form</div>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className='mb-4 mt-3.5'>
+        <label
+          htmlFor='name'
+          className='mb-2 block text-base font-medium text-black'
+        >
+          Name
+        </label>
+        <input
+          type='text'
+          placeholder='Name'
+          className='w-full rounded-md border border-gray-300 py-3 px-6'
+          {...register('name', { required: true })}
+        />
+      </div>
+      <div className='mb-4'>
+        <label
+          htmlFor='email'
+          className='mb-2 block text-base font-medium text-black'
+        >
+          Email
+        </label>
+        <input
+          type='email'
+          placeholder='example@domain.com'
+          className='w-full rounded-md border border-gray-300 py-3 px-6'
+          {...register('email', { required: true })}
+        />
+      </div>
+      <div className='mb-4'>
+        <label
+          htmlFor='message'
+          className='mb-2 block text-base font-medium text-black'
+        >
+          Message
+        </label>
+        <textarea
+          rows={4}
+          placeholder='Type your message'
+          className='resize-none w-full rounded-md border border-gray-300 py-3 px-6'
+          {...register('message', { required: true })}
+        ></textarea>
+      </div>
+      <button className='text-white rounded-md bg-orange px-3 py-2 hover:bg-orange-200'>
+        Submit
+      </button>
+    </form>
   )
 }
---
+++ app/utils/sendEmail.ts
@@ -0,0 +1,17 @@
+import { ContactFormData } from '@/components/contact-form';
+
+export function sendEmail(data: ContactFormData) {
+  const apiEndpoint = '/api/email';
+
+  fetch(apiEndpoint, {
+    method: 'POST',
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      alert(response.message);
+    })
+    .catch((err) => {
+      alert(err);
+    });
+}
