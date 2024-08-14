3a976ffda3873e36dca4b480f40c79f50d9809b1
Cody Hasty <codyrhasty@gmail.com>
2024-08-13 20:04:21 -0700

    fix: add responsive styling to company logos

--- app/components/experience/company-logos.tsx
+++ app/components/experience/company-logos.tsx
@@ -1,19 +1,19 @@
 import Image from 'next/image'
-import tlLogo from '@public/images/experience/Instrumentl-Logo.png'
-import pandoraLogo from '@public/images/experience/Pandora-Logo.png'
-import volleyLogo from '@public/images/experience/Volley-Logo.png'
-import mymicLogo from '@public/images/experience/MYMIC-Logo.png'
-import oduLogo from '@public/images/experience/ODU-Logo.png'
+import tlLogo from '@images/experience/Instrumentl-Logo.png'
+import pandoraLogo from '@images/experience/Pandora-Logo.png'
+import volleyLogo from '@images/experience/Volley-Logo.png'
+import mymicLogo from '@images/experience/MYMIC-Logo.png'
+import oduLogo from '@images/experience/ODU-Logo.png'
 
 export default function CompanyLogos() {
   return (
-    <div className='mt-4 w-full'>
-      <div id='companyLogos' className='flex flex-row gap-8 items-center max-w-full'>
-        <div className='w-full'><Image src={tlLogo} alt="Instrumentl Logo" /></div>
-        <div className='w-full'><Image src={pandoraLogo} alt="Pandora Logo" /></div>
-        <div className='w-full'><Image src={volleyLogo} alt="Volley Logo" /></div>
-        <div className='w-full'><Image src={mymicLogo} alt="MYMIC Logo" /></div>
-        <div className='w-full'><Image src={oduLogo} alt="ODU Logo" /></div>
+    <div className='mt-4 w-full mb-4'>
+      <div id='companyLogos' className='flex flex-row flex-wrap gap-4 items-center justify-around max-w-full'>
+        <div className='max-w-[174px]'><Image src={tlLogo} alt="Instrumentl Logo" /></div>
+        <div className='max-w-[174px]'><Image src={pandoraLogo} alt="Pandora Logo" /></div>
+        <div className='max-w-[174px]'><Image src={volleyLogo} alt="Volley Logo" /></div>
+        <div className='max-w-[174px]'><Image src={mymicLogo} alt="MYMIC Logo" /></div>
+        <div className='max-w-[174px]'><Image src={oduLogo} alt="ODU Logo" /></div>
       </div>
     </div>
   )
