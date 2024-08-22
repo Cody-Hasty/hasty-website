0c4f6e5d81bef685b8c12ad3894529b9c0f23a41
Cody Hasty <codyrhasty@gmail.com>
2024-08-21 21:03:26 -0700

    fix: remove more stories section when there are none

--- app/components/blog/partials/more-stories.tsx
+++ app/components/blog/partials/more-stories.tsx
@@ -7,22 +7,24 @@ type MoreStoriesProps = {
 
 export default function MoreStories({ posts }: MoreStoriesProps) {
   return (
-    <div>
-      <h2 className="mb-8 text-4xl font-bold tracking-tighter leading-tight">
-        More Stories
-      </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-16 md:gap-y-32 mb-6">
-        {posts.map((post) => (
-          <PostPreview
-            key={post.slug}
-            title={post.title}
-            blogImage={post.blogImage}
-            date={post.date}
-            slug={post.slug}
-            excerpt={post.excerpt}
-          />
-        ))}
+    posts.length > 0 && (
+      <div>
+        <h2 className="mb-8 text-4xl font-bold tracking-tighter leading-tight">
+          More Stories
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-16 md:gap-y-32 mb-6">
+          {posts.map((post) => (
+            <PostPreview
+              key={post.slug}
+              title={post.title}
+              blogImage={post.blogImage}
+              date={post.date}
+              slug={post.slug}
+              excerpt={post.excerpt}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    )
   )
 }
