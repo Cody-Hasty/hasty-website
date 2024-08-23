f25e0affc52ea07184c5fd8e8617aa4a9633e82f
Cody Hasty <codyrhasty@gmail.com>
2024-08-22 23:57:10 -0700

    feat: fill in about blurb for the homepage

--- app/components/about/about-blurb.tsx
+++ app/components/about/about-blurb.tsx
@@ -2,10 +2,18 @@ import { RiDoubleQuotesL, RiDoubleQuotesR } from "react-icons/ri";
 
 export default function AboutBlurbComponent() {
   return (
-    <blockquote className="blockquote m-0 flex flex-col">
-      <RiDoubleQuotesL className="me-auto mb-2" />
-      <p className="text-center m-0">Hey, I&apos;m Cody Hasty. I&apos;m a software engineer based in San Francisco.</p>
-      <RiDoubleQuotesR className="ms-auto mt-2" />
-    </blockquote>
+    <div className="mt-4 flex flex-col items-center justify-center">
+      <blockquote className="blockquote m-0 flex flex-col">
+        <RiDoubleQuotesL className="me-auto mb-2" />
+        <div className="my-0 mx-4 flex flex-col gap-2">
+          <p>I&apos;m a software engineer in San Francisco with a focus in full stack applications utilizing Typescript, React, and Next.js. Making websites beautiful and intuitive is my bread and butter, and finding some unique spark to make them stand apart is my passion.</p>
+          <p>When I&apos;m not wrangling API calls, I enjoy dabbling in artistic ventures of all varieties: 3D printing, three.js, React data visualization, creating Unity games, Blender, Photoshop, and most recently - whittling!</p>
+          <p>I&apos;m always looking for new opportunities to learn and grow, so if you have a project you think I&apos;d be a good fit for, please don&apos;t hesitate to reach out!</p>
+          <p>To see some of my past work, please visit my <a href="/about/experience">Experience</a> or <a href="/about/github">Github</a> pages.</p>
+        </div>
+        <RiDoubleQuotesR className="ms-auto mt-2" />
+        <i className="blockquote-footer mt-3 ml-4">Cody Hasty</i>
+      </blockquote>
+    </div>
   )
 }
