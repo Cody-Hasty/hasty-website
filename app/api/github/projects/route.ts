637c24caede74ade538fc9fa2d2d7a0dabb0f19a
Cody Hasty <codyrhasty@gmail.com>
2024-08-24 12:55:00 -0700

    feat: default sort Github repositories by last updated

--- app/api/github/projects/route.ts
+++ app/api/github/projects/route.ts
@@ -1,8 +1,20 @@
 import { type NextRequest, NextResponse } from "next/server";
 
+function projectsSortedBy(sortPreference: string, projects: any[]) {
+  if (sortPreference === "updated") {
+    return projects.sort((a, b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime())
+  } else if (sortPreference === "created") {
+    return projects.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime())
+  } else if (sortPreference === "alphabetical") {
+    return projects.sort((a, b) => a.name.localeCompare(b.name))
+  } else {
+    return projects
+  }
+}
+
 export async function GET(request: NextRequest, response: NextResponse) {
   const projects = await fetch("https://api.github.com/users/Cody-Hasty/repos")
     .then(res => res.json())
     .catch(() => null)
-  return NextResponse.json(projects, { status: projects ? 200 : 500 })
+  return NextResponse.json(projectsSortedBy("updated", projects), { status: projects ? 200 : 500 })
 }
