e484b418dccadaf4643befe60d4f79af4a75d526
Cody Hasty <codyrhasty@gmail.com>
2024-08-24 12:30:29 -0700

    style: override default font size and spacing

--- app/components/experience/timeline-element.tsx
+++ app/components/experience/timeline-element.tsx
@@ -12,7 +12,7 @@ type TimelineElementProps = {
 export default function TimelineElementComponent({ title, location, company, image, highlights = [] }: TimelineElementProps) {
   return (
     <div>
-      <div className="flex flex-row gap-x-6">
+      <div className="flex flex-row gap-x-6 gap-y-2 flex-wrap-reverse">
         <div className="basis-3/4">
           <h5 className="m-0 mb-2">{`${title} @ ${company}`}</h5>
           <p className="text-neutral-500 m-0 mb-2">{location}</p>
@@ -23,6 +23,7 @@ export default function TimelineElementComponent({ title, location, company, ima
           </div>
         }
       </div>
+      <hr />
       <ul className="list-disc">
         {highlights.map((highlight: string, index: number) => (
           <li key={index} className="text-neutral-500">{highlight}</li>
--- app/components/shared/page-header.tsx
+++ app/components/shared/page-header.tsx
@@ -9,7 +9,7 @@ type PageHeaderProps = {
 export default function PageHeaderComponent({ mainText, description }: PageHeaderProps) {
   return (
     <div className="flex flex-col items-center gap-2 p-4 bg-[darkblue]/70  bg-gradient-to-r from-[darkblue]/60 to-main-blue/80 rounded-t-2xl">
-      <h1 className={`${font.className} text-white`}>{mainText}</h1>
+      <h1 className={`${font.className} text-white mb-0 text-4xl`}>{mainText}</h1>
       { description && <p className="m-0 text-white">{description}</p> }
     </div>
   )
